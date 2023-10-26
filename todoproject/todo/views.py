1b8c87b8b0c86f1c58da2ddffbcde4800255871c
Nadil <115153495+nadiltalrop@users.noreply.github.com>
2023-10-26 10:47:26 +0530

    Update views.py

--- todoproject/todo/views.py
+++ todoproject/todo/views.py
@@ -41,4 +41,56 @@ class TaskDeleteView(DeleteView):
 
     def get_success_url(self):
         return reverse_lazy('todo:home')
+
+
+# function based 
+
+def home(request):
+     
+    tasks = Task.objects.all()
+    print(tasks)
+
+   
+    if request.method == 'POST':
+        name = request.POST.get('task')
+        priority = request.POST.get('priority')
+        date = request.POST.get('date')
+
+        tasks = Task.objects.create(name=name, priority=priority,date=date)
+        tasks.save()
+        return redirect('todo:home')
+    
+    context = {
+        'tasks': tasks
+    }
+
+    return render(request, 'home.html',context=context)
+
+
+def delete(request, id):
+
+    if request.method == 'POST':
+        if Task.objects.filter(id=id).exists():
+            task = Task.objects.get(id=id)
+            task.delete()
+            return redirect('todo:home')
+        else:
+            pass
+    
+    return render(request,'delete.html')
+
+
+def update(request, id):
+    task = Task.objects.get(id=id)
+    form = TaskForm(request.POST or None, instance=task)
+
+    if form.is_valid():
+        form.save()
+        return redirect('todo:home')
+    
+    context = {
+        'form': form
+    }
+
+    return render(request, 'update.html', context=context)
     
