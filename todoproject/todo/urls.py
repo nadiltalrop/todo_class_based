b42f3adaa99e83a0c7005453be1783b75c17ff51
Nadil <115153495+nadiltalrop@users.noreply.github.com>
2023-10-26 10:46:49 +0530

    Update urls.py

--- todoproject/todo/urls.py
+++ todoproject/todo/urls.py
@@ -6,7 +6,9 @@ from. import views
 app_name = 'todo'
 
 urlpatterns = [
-
+    path('',views.home,name='home'),
+    path('delete/<int:id>/',views.delete,name='delete'),
+    path('update/<int:id>/',views.update,name='update'),
     path('',views.TaskListView.as_view(),name='home'),
     path('cvdetails/<int:pk>/',views.TaskDetailView.as_view(),name='cvdetails'),
     path('cvupdate/<int:pk>/',views.TaskUpdateView.as_view(),name='cvupdate'),
